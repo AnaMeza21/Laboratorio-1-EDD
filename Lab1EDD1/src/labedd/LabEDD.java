6f761d35e71ab62d3b0591b3b447b6d40c5e9a66
AnaMeza21 <mezaana@uninorte.edu.co>
2025-03-29 12:53:35 -0500

    Laboratorio

---
+++ Lab1EDD1/src/labedd/LabEDD.java
@@ -0,0 +1,12 @@
+package labedd;
+
+public class LabEDD {
+
+    public static void main(String[] args) {
+       Login Lg = new Login();
+       Lg.setSize (900,500);
+       Lg.setLocationRelativeTo(null);
+       Lg.setVisible(true);
+    }
+    
+}
